f08f3d959df2087e1ead223e645b89a395bbdac5
stefanvilce <stefan@irisoft.com.ro>
2019-10-11 15:14:53 +0200

    INdexController modified

--- src/main/java/com/corbeni/reactweb/controller/IndexController.java
+++ src/main/java/com/corbeni/reactweb/controller/IndexController.java
@@ -1,17 +1,15 @@
 package com.corbeni.reactweb.controller;
 
-//import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) {
-    	model.addAttribute("cssPath", Helpers.cssPath);
+    	model.addAttribute("cssPath", Helpers.cssPath);    	
     	model.addAttribute("jsPath", Helpers.jsPath);
     	model.addAttribute("title", "My next holiday");
         model.addAttribute("subtitle", "Here I would like travel soon. As soon as possible");
